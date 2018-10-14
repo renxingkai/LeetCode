a084a7b82e914d386b99faaa478de312c97c3ba2
renxingkai <179049243@qq.com>
2018-10-14 22:12:59 +0800

    Convert a Number to Hexadecimal

---
+++ LeetCode_405.java
@@ -0,0 +1,18 @@
+public class LeetCode_405 {
+
+    public static void main(String[] arg) {
+        System.out.println(toHex(-1));
+    }
+
+    /**
+     * int类型转为16进制
+     *
+     * @param num
+     * @return
+     */
+    public static String toHex(int num) {
+        return Integer.toHexString(num);
+    }
+
+
+}
